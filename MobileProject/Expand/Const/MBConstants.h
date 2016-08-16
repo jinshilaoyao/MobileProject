4fb3c644eb1067a7621c3c3809df4ab2e33419b9
jinshilaoyao <1145119186@qq.com>
2016-08-16 10:50:59 +0800

    no message

---
+++ MobileProject/Expand/Const/MBConstants.h
@@ -0,0 +1,17 @@
+//
+//  MBConstants.h
+//  MobileProject 常量
+//
+//  Created by wujunyang on 16/6/13.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#ifndef MBConstants_h
+#define MBConstants_h
+
+//邮件接收人
+static NSString* const kMail_cc_ToRecipients_Address=@"mobileproject@126.com";
+
+
+
+#endif /* MBConstants_h */
---
+++ MobileProject/Main/Preview/Controller/MPMoreViewController.h
@@ -0,0 +1,30 @@
+//
+//  MPMoreViewController.h
+//  MobileProject 导航
+//
+//  Created by wujunyang on 16/7/20.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+#import "UIView+Extension.h"
+#import "JSPatchViewController.h"
+#import "MPLkdbViewController.h"
+#import "BaiDuMapViewController.h"
+#import "MPQRCodeViewController.h"
+#import "MPUploadImagesViewController.h"
+#import "MPUploadWithPropressViewController.h"
+#import "MPAdaptationFontViewController.h"
+#import "LoggerViewController.h"
+#import "MPReduceTimeViewController.h"
+#import "MPWebViewController.h"
+#import "MPChildrenViewController.h"
+#import "MPBlankPageViewController.h"
+#import "MPAddressPickViewController.h"
+#import "MPCustomAlertViewController.h"
+#import "MPYYTextViewController.h"
+
+@interface MPMoreViewController : BaseViewController
+
+@end
